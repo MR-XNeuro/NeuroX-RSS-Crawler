db649cfcd4e43b93735d75380aa38732ae1ce1e0
MR-XNeuro <mtnsohrabi@gmail.com>
2025-08-07 18:52:27 +0330

    Update neurox_crawler.py

--- neurox_crawler.py
+++ neurox_crawler.py
@@ -146,3 +146,9 @@ if __name__ == "__main__":
     flask_thread.daemon = True
     flask_thread.start()
     loop_runner()
+    @app.route('/crawl-now', methods=["GET"])
+def trigger_crawler():
+    print("📡 Webhook triggered manually")
+    main()
+    return "✅ Crawler triggered successfully"
+
